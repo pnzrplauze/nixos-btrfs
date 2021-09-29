eaf48b0d0bd12199f8956355519fba775f030314
Jack <hadesranger@gmail.com>
2021-09-29 12:14:18 +0200

    sdfasd

--- hosts/nixos/hardware.nix
+++ hosts/nixos/hardware.nix
@@ -5,16 +5,18 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = with config.boot.kernelPackages; [ linuxPackages_zen.rtw89 ];
+  boot.extraModulePackages = [ config.boot.kernelPackages.rtw89 ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/2f281ab2-85c7-4dd1-af7d-0b38962e29ef";
+    {
+      device = "/dev/disk/by-uuid/2f281ab2-85c7-4dd1-af7d-0b38962e29ef";
       fsType = "btrfs";
       options = [ "subvol=root" "compress=zstd" "noatime" ];
     };
@@ -22,19 +24,20 @@
   boot.initrd.luks.devices."enc".device = "/dev/disk/by-uuid/80d32392-f4cf-4967-b1ff-b2af40113071";
 
   fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/2f281ab2-85c7-4dd1-af7d-0b38962e29ef";
+    {
+      device = "/dev/disk/by-uuid/2f281ab2-85c7-4dd1-af7d-0b38962e29ef";
       fsType = "btrfs";
       options = [ "subvol=home" "compress=zstd" "noatime" ];
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/B8FC-0203";
+    {
+      device = "/dev/disk/by-uuid/B8FC-0203";
       fsType = "vfat";
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/a3b4a015-dec7-424e-a3fa-263000a75845"; }
-    ];
+    [{ device = "/dev/disk/by-uuid/a3b4a015-dec7-424e-a3fa-263000a75845"; }];
 
   hardware.cpu.amd.updateMicrocode = true;
 
