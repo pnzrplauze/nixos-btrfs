7b7ea8c0c6a8cca8a9c8a93ccf9802dd6139aa54
Jack <hadesranger@gmail.com>
2021-10-01 20:34:29 +0200

    neovim

--- profiles/develop/nvim/default.nix
+++ profiles/develop/nvim/default.nix
@@ -16,65 +16,65 @@ in
   # ];
   programs.neovim = {
     enable = true;
-    package = nixos-unstable.neovim-unwrapped;
+    # package = nixos-unstable.neovim-unwrapped;
     viAlias = true;
     vimAlias = true;
     vimdiffAlias = true;
-    plugins = with nixos-unstable.vimPlugins; [
-      # csv-vim
-      # vim-surround  # fix config
-      # vim-repeat
-      # # vim-speeddating  # makes statusline buggy??
-      # vim-commentary
-      # vim-unimpaired
-      # vim-sleuth  # adjusts shiftwidth and expandtab based on the current file
-      # vim-startify
-      # vim-multiple-cursors
-      # gundo-vim
-      # vim-easy-align
-      # vim-table-mode
-      # editorconfig-vim
-      # vim-markdown
-      # ansible-vim
-      # vim-nix
-      # robotframework-vim
-      # # vimspector
-      # vim-plugins.vim-bepoptimist
-      # vim-plugins.nvim-base16  # the one packaged in nixpkgs is different
-      # popup-nvim
-      # plenary-nvim
-      # telescope-nvim
-      # telescope-symbols-nvim
-      # # telescope-media-files  # doesn't support wayland yet
-      # nvim-colorizer-lua
-      # nvim-treesitter
-      # nvim-lspconfig
-      # lsp_extensions-nvim
-      # # completion-nvim
-      # nvim-compe
-      # lspkind-nvim
-      # gitsigns-nvim
-      # bufferline-nvim
-      # nvim-autopairs
-      # galaxyline-nvim
-      # vim-closetag
-      # friendly-snippets
-      # vim-vsnip
-      # nvim-tree-lua
-      # nvim-web-devicons
-      # vim-devicons
-      # # vim-auto-save  # ?
-      # vim-plugins.neoscroll-nvim
-      # vim-plugins.zenmode-nvim
-      # # minimap-vim
-      # vim-plugins.indent-blankline-nvim  # using my own derivation because the nixpkgs still uses the master branch
-      # vim-easymotion
-      # quick-scope
-      # matchit-zip
-      # targets-vim
-      # neoformat
-      # vim-numbertoggle
-    ];
+    # plugins = with nixos-unstable.vimPlugins; [
+    #    csv-vim
+    #    vim-surround  # fix config
+    #    vim-repeat
+    #    # vim-speeddating  # makes statusline buggy??
+    #    vim-commentary
+    #    vim-unimpaired
+    #    vim-sleuth  # adjusts shiftwidth and expandtab based on the current file
+    #    vim-startify
+    #    vim-multiple-cursors
+    #    gundo-vim
+    #    vim-easy-align
+    #    vim-table-mode
+    #    editorconfig-vim
+    #    vim-markdown
+    #    ansible-vim
+    #    vim-nix
+    #    robotframework-vim
+    #    # vimspector
+    #    vim-plugins.vim-bepoptimist
+    #    vim-plugins.nvim-base16  # the one packaged in nixpkgs is different
+    #    popup-nvim
+    #    plenary-nvim
+    #    telescope-nvim
+    #    telescope-symbols-nvim
+    #    # telescope-media-files  # doesn't support wayland yet
+    #    nvim-colorizer-lua
+    #    nvim-treesitter
+    #    nvim-lspconfig
+    #    lsp_extensions-nvim
+    #    # completion-nvim
+    #    nvim-compe
+    #    lspkind-nvim
+    #    gitsigns-nvim
+    #    bufferline-nvim
+    #    nvim-autopairs
+    #    galaxyline-nvim
+    #    vim-closetag
+    #    friendly-snippets
+    #    vim-vsnip
+    #    nvim-tree-lua
+    #    nvim-web-devicons
+    #    vim-devicons
+    #    # vim-auto-save  # ?
+    #    vim-plugins.neoscroll-nvim
+    #    vim-plugins.zenmode-nvim
+    #    # minimap-vim
+    #    vim-plugins.indent-blankline-nvim  # using my own derivation because the nixpkgs still uses the master branch
+    #    vim-easymotion
+    #    quick-scope
+    #    matchit-zip
+    #    targets-vim
+    #    neoformat
+    #    vim-numbertoggle
+    # ];
 
     extraConfig = "lua << EOF\n" + builtins.readFile ./init.lua + "\nEOF";
   };
