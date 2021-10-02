b34d6db3d23d6b6f35469539c1ba5b61b38a0da2
Jack <hadesranger@gmail.com>
2021-10-02 23:38:25 +0200

    sdafasd

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -21,50 +21,58 @@
     snapshotInterval = "hourly";
     cleanupInterval = "1d";
     configs = {
-      "home": {
-      "subvolume": "/home",
-      "extraConfig": "ALLOW_USERS=\"jack\"\nTIMELINE_CREATE=yes\nTIMELINE_CLEANUP=yes\n"
-      }
-      "root": {
-      "subvolume": "/root",
-      "extraConfig": "ALLOW_USERS=\"root\"\nTIMELINE_CREATE=yes\nTIMELINE_CLEANUP=yes\n"
-      }
+      home = {
+        subvolume = "/home";
+        extraConfig = ''
+          ALLOW_USERS="jack"
+          TIMELINE_CREATE=yes
+          TIMELINE_CLEANUP=yes
+        '';
       };
+      root = {
+        subvolume = "/root";
+        extraConfig = ''
+          ALLOW_USERS="root"
+          TIMELINE_CREATE=yes
+          TIMELINE_CLEANUP=yes
+        '';
       };
+    };
+  };
 
-      environment.systemPackages = with pkgs; [
-        xorg.setxkbmap
-        xorg.xset
-        brightnessctl
-      ];
-      # services.udev.extraRules = ''
-      #   SUBSYSTEM=="usb", ATTR{idVendor}=="3297", GROUP="input"
-      #   # Rule for the Moonlander
-      #   SUBSYSTEM=="usb", ATTR{idVendor}=="3297", ATTR{idProduct}=="1969", GROUP="input"
-      # '';
+  environment.systemPackages = with pkgs; [
+    xorg.setxkbmap
+    xorg.xset
+    brightnessctl
+  ];
+  # services.udev.extraRules = ''
+  #   SUBSYSTEM=="usb", ATTR{idVendor}=="3297", GROUP="input"
+  #   # Rule for the Moonlander
+  #   SUBSYSTEM=="usb", ATTR{idVendor}=="3297", ATTR{idProduct}=="1969", GROUP="input"
+  # '';
 
-      boot.loader.systemd-boot.enable = true;
-      boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
-      # For Redshift - not currently used
-      # location = {
-      #   latitude = 38.833881;
-      #   longitude = -104.821365;
-      # };
+  # For Redshift - not currently used
+  # location = {
+  #   latitude = 38.833881;
+  #   longitude = -104.821365;
+  # };
 
-      # networking.useDHCP = false;
+  # networking.useDHCP = false;
 
-      nix.maxJobs = lib.mkDefault 16;
+  nix.maxJobs = lib.mkDefault 16;
 
-      services.xserver.videoDrivers = [ "amd" ];
+  services.xserver.videoDrivers = [ "amd" ];
 
-      # services.fstrim.enable = true;
+  # services.fstrim.enable = true;
 
-      services.picom = {
-        backend = "glx";
-        vSync = true;
-      };
+  services.picom = {
+    backend = "glx";
+    vSync = true;
+  };
 
-      security.mitigations.acceptRisk = true;
+  security.mitigations.acceptRisk = true;
 
-    }
+}
